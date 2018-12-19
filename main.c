87dd54241e9f8297c9c9c799b5efa942e1ba7132
Verena Brewe <verenabrewe@web.de>
2018-12-19 22:15:56 +0100

    Schaltjahresrechner

--- main.c
+++ main.c
@@ -1,7 +1,7 @@
 /**
- *
- *
- *
+ *Titel:
+ *Autor: Verena Brewe
+ *Datum: 18.12.2018
  **/
 
 #include <stdio.h>
@@ -11,6 +11,7 @@
  *
  *
  **/
+int isLeapyear(int);
 int day_of_the_year(int day, int month, int year)
 {
 
@@ -20,5 +21,44 @@ int day_of_the_year(int day, int month, int year)
 int main()
 {
     printf("Tag des Jahres: %i\n", day_of_the_year(31, 12, 2018));
+    printf("Berechnung eines Schaltjahres\n");
+
+    int jahreszahl = 0;
+
+    printf("Geben Sie eine Jahreszahl ein: \n");
+    scanf("%i", & jahreszahl);
+    isLeapyear(jahreszahl);
+    return 0;
+}
+int isLeapyear(int jahreszahl)
+{
+    if (jahreszahl < 1582)
+    {
+        printf("Dieses Jahr liegt ausserhalb des gregorianischen Kalenders\n");
+        return -1;
+    }
+    else if (jahreszahl % 4 == 0)
+    {
+        if (jahreszahl % 100 == 0)
+        {
+            if (jahreszahl % 400 == 0)
+            {
+                printf("Dieses Jahr ist ein Schaltjahr\n");
+            }
+            else
+            {
+                printf("Dieses Jahr ist kein Schaltjahr\n");
+            }
+        }
+        else
+        {
+            printf("Dieses Jahr ist ein Schaltjahr\n");
+        }
+    }
+
+    else
+    {
+        printf("Dieses Jahr ist kein Schaltjahr\n");
+    }
     return 0;
 }
